bca7caa309f3a4c360ffa2d8183c8846cc898c7b
IYO <IYO.MAIL.CN@GMAIL.COM>
2024-01-31 17:32:25 +0800

    修补Bug,添加错误捕获

--- src/main/java/com/iyo/BootStrap.java
+++ src/main/java/com/iyo/BootStrap.java
@@ -4,6 +4,7 @@ import com.iyo.main.SysMain;
 import com.iyo.util.ExceptionUtil;
 
 public class BootStrap {
+    //主程序入口
     public static void main(String[] args) {
         try {
             SysMain.Start();
